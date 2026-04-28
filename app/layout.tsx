a1d54d2b35eda891c911da9df91c7d9d57be966d
bahrouni-ayoub <Ayoub.BAHROUNI@esprit.tn>
2026-04-28 23:03:52 +0100

    setup the app and implement the home page and new book ui.

---
+++ app/layout.tsx
@@ -0,0 +1,54 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono, IBM_Plex_Serif, Mona_Sans } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
+
+import "./globals.css";
+import Navbar from "@/components/Navbar";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const ibmPlexSerif = IBM_Plex_Serif({
+  variable: "--font-ibm-plex-serif",
+  subsets: ['latin'],
+  weight: ['400', "500", "600", "700"],
+  display: "swap"
+})
+
+const monoSans = Mona_Sans({
+  variable: "--font-mona-sans",
+  subsets: ['latin'],
+  display: "swap"
+})
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "PagePal",
+  description: "Transform your Books into an interactive AI conversations. Upload PDFs, and chat with you book using voice.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="en"
+      className={`${ibmPlexSerif.variable} ${monoSans.variable} h-full relative font-sans antialiased`}
+    >
+      <body className="min-h-full flex flex-col">
+        <ClerkProvider>
+          <Navbar />
+          {children}
+        </ClerkProvider>
+      </body>
+    </html>
+  );
+}
